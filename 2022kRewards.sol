676eb817f16e27d9583beb0979fdf75108fad5bf
Diamondzminde <93412145+Diamondzminde@users.noreply.github.com>
2022-06-27 17:34:50 -0700

    Update 2022kRewards.sol

--- 2022kRewards.sol
+++ 2022kRewards.sol
@@ -1,36 +1,165 @@
 // SPDX-License-Identifier: MIT LICENSE
 
 pragma solidity 0.8.4;
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract 2022kRewards is ERC20, ERC20Burnable, Ownable {
+import "https://github.com/Diamondzminde/nftstaking/blob/main/2022kRewards.sol"
+import "https://github.com/Diamondzminde/nftstaking/blob/main/Collection.sol"
 
-  mapping(address => bool) controllers;
+contract NFTStaking is Ownable, IERC721Receiver {
+
+  uint256 public totalStaked;
   
-  constructor() ERC20("2022kRewards", "2022k") { }
-
-  function mint(address to, uint256 amount) external {
-    require(controllers[msg.sender], "Only controllers can mint");
-    _mint(to, amount);
+  // struct to store a stake's token, owner, and earning values
+  struct Stake {
+    uint24 tokenId;
+    uint48 timestamp;
+    address owner;
   }
 
-  function burnFrom(address account, uint256 amount) public override {
-      if (controllers[msg.sender]) {
-          _burn(account, amount);
+  event NFTStaked(address owner, uint256 tokenId, uint256 value);
+  event NFTUnstaked(address owner, uint256 tokenId, uint256 value);
+  event Claimed(address owner, uint256 amount);
+
+  // reference to the Block NFT contract
+  Collection nft;
+  2022kRewards token;
+
+  // maps tokenId to stake
+  mapping(uint256 => Stake) public vault; 
+
+   constructor(Collection _nft, 2022kRewards _token) { 
+    nft = _nft;
+    token = _token;
+  }
+
+  function stake(uint256[] calldata tokenIds) external {
+    uint256 tokenId;
+    totalStaked += tokenIds.length;
+    for (uint i = 0; i < tokenIds.length; i++) {
+      tokenId = tokenIds[i];
+      require(nft.ownerOf(tokenId) == msg.sender, "not your token");
+      require(vault[tokenId].tokenId == 0, 'already staked');
+
+      nft.transferFrom(msg.sender, address(this), tokenId);
+      emit NFTStaked(msg.sender, tokenId, block.timestamp);
+
+      vault[tokenId] = Stake({
+        owner: msg.sender,
+        tokenId: uint24(tokenId),
+        timestamp: uint48(block.timestamp)
+      });
+    }
+  }
+
+  function _unstakeMany(address account, uint256[] calldata tokenIds) internal {
+    uint256 tokenId;
+    totalStaked -= tokenIds.length;
+    for (uint i = 0; i < tokenIds.length; i++) {
+      tokenId = tokenIds[i];
+      Stake memory staked = vault[tokenId];
+      require(staked.owner == msg.sender, "not an owner");
+
+      delete vault[tokenId];
+      emit NFTUnstaked(account, tokenId, block.timestamp);
+      nft.transferFrom(address(this), account, tokenId);
+    }
+  }
+
+  function claim(uint256[] calldata tokenIds) external {
+      _claim(msg.sender, tokenIds, false);
+  }
+
+  function claimForAddress(address account, uint256[] calldata tokenIds) external {
+      _claim(account, tokenIds, false);
+  }
+
+  function unstake(uint256[] calldata tokenIds) external {
+      _claim(msg.sender, tokenIds, true);
+  }
+
+  function _claim(address account, uint256[] calldata tokenIds, bool _unstake) internal {
+    uint256 tokenId;
+    uint256 earned = 0;
+
+    for (uint i = 0; i < tokenIds.length; i++) {
+      tokenId = tokenIds[i];
+      Stake memory staked = vault[tokenId];
+      require(staked.owner == account, "not an owner");
+      uint256 stakedAt = staked.timestamp;
+      earned += 100000 ether * (block.timestamp - stakedAt) / 1 days;
+      vault[tokenId] = Stake({
+        owner: account,
+        tokenId: uint24(tokenId),
+        timestamp: uint48(block.timestamp)
+      });
+
+    }
+    if (earned > 0) {
+      earned = earned / 10;
+      token.mint(account, earned);
+    }
+    if (_unstake) {
+      _unstakeMany(account, tokenIds);
+    }
+    emit Claimed(account, earned);
+  }
+
+  function earningInfo(uint256[] calldata tokenIds) external view returns (uint256[2] memory info) {
+     uint256 tokenId;
+     uint256 totalScore = 0;
+     uint256 earned = 0;
+      Stake memory staked = vault[tokenId];
+      uint256 stakedAt = staked.timestamp;
+      earned += 100000 ether * (block.timestamp - stakedAt) / 1 days;
+    uint256 earnRatePerSecond = totalScore * 1 ether / 1 days;
+    earnRatePerSecond = earnRatePerSecond / 100000;
+    // earned, earnRatePerSecond
+    return [earned, earnRatePerSecond];
+  }
+
+  // should never be used inside of transaction because of gas fee
+  function balanceOf(address account) public view returns (uint256) {
+    uint256 balance = 0;
+    uint256 supply = nft.totalSupply();
+    for(uint i = 1; i <= supply; i++) {
+      if (vault[i].owner == account) {
+        balance += 1;
       }
-      else {
-          super.burnFrom(account, amount);
+    }
+    return balance;
+  }
+
+  // should never be used inside of transaction because of gas fee
+  function tokensOfOwner(address account) public view returns (uint256[] memory ownerTokens) {
+
+    uint256 supply = nft.totalSupply();
+    uint256[] memory tmp = new uint256[](supply);
+
+    uint256 index = 0;
+    for(uint tokenId = 1; tokenId <= supply; tokenId++) {
+      if (vault[tokenId].owner == account) {
+        tmp[index] = vault[tokenId].tokenId;
+        index +=1;
       }
+    }
+
+    uint256[] memory tokens = new uint256[](index);
+    for(uint i = 0; i < index; i++) {
+      tokens[i] = tmp[i];
+    }
+
+    return tokens;
   }
 
-  function addController(address controller) external onlyOwner {
-    controllers[controller] = true;
-  }
-
-  function removeController(address controller) external onlyOwner {
-    controllers[controller] = false;
-  }
+  function onERC721Received(
+        address,
+        address from,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+      require(from == address(0x0), "Cannot send nfts to Vault directly");
+      return IERC721Receiver.onERC721Received.selector;
+    }
+  
 }
